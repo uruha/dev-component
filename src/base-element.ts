668ca4d9200f6bf475f045bf006d789659909ed9
uruha <kochmusica@gmail.com>
2018-07-01 22:56:50 +0900

    add custom elements

---
+++ src/base-element.ts
@@ -0,0 +1,44 @@
+import { render } from 'lit-html/lib/lit-extended';
+import { TemplateResult } from 'lit-html';
+
+const connected = new WeakMap();
+
+export class BaseElement extends HTMLElement {
+    get connected() {
+        return connected.get(this);
+    }
+
+    connectedCallback(update: boolean = true) {
+        connected.set(this, true);
+        if (update) {
+            this.update();
+        }
+    }
+
+    disconnectedCallback() {
+        connected.delete(this);
+    }
+
+    private _render(template: TemplateResult) {
+        render(
+            template,
+            this.shadowRoot || this.attachShadow({ mode: 'open' })
+        );
+        this.renderedCallback();
+    }
+
+    protected update() {
+        const template = this.render();
+        if (template) {
+            this._render(template);
+        }
+    }
+
+    protected render(): TemplateResult {
+        throw new Error('Render function not Implemented');
+    }
+
+    protected renderedCallback() {
+        // Empty
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,51 @@
-class Greeter {
-    greeting: string;
-    constructor(message: string) {
-        this.greeting = message;
+import { BaseElement } from './base-element';
+import { html } from 'lit-html/lib/lit-extended';
+import define from './define';
+
+const ATTR = {
+    DATA_NAME: 'data-name'
+};
+
+const name = new WeakMap();
+
+class Hello extends BaseElement {
+    static get observedAttributes() {
+        return [ATTR.DATA_NAME];
     }
-    greet() {
-        return `Hello, ${this.greeting}`;
+
+    constructor() {
+        super();
+    }
+
+    attributeChangedCallback(attr: string, prev: any, next: any) {
+        if (prev === next) {
+            return;
+        }
+        switch (attr) {
+            case ATTR.DATA_NAME:
+                name.set(this, next);
+                break;
+            default:
+        }
+        if (this.connected) {
+            this.update();
+        }
+    }
+
+    render() {
+        const { n } = { n: name.get(this) };
+        return html`
+        <style>
+        div {
+            padding: 14px;
+            margin: 0;
+        }
+        </style>
+        <div>
+            Hello ${n}
+        </div>
+        `;
     }
 }
 
-const greeter = new Greeter('Typescript');
-console.log(greeter.greet());
+define('hello-tag', Hello);
